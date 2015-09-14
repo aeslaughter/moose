8563938ef86134ad2218650ea39258f53d2e08d3
Andrew E Slaughter <andrew.slaughter@inl.gov>
2015-09-14 10:40:00 -0600

    Basic script

--- framework/src/controls/ChaiControlBase.C
+++ framework/src/controls/ChaiControlBase.C
@@ -18,6 +18,10 @@
 // ChaiScript requires Cxx11
 #ifdef LIBMESH_HAVE_CXX11
 
+void callafunc(const std::function<void (const std::string &)> &t_func)
+{
+  t_func("bob");
+}
 
 template<>
 InputParameters validParams<ChaiControlBase>()
@@ -31,17 +35,31 @@ ChaiControlBase::ChaiControlBase(const InputParameters & parameters) :
     Control(parameters)
 {
 
-  chaiscript::ChaiScript chai(chaiscript::Std_Lib::library());
 
 }
 
 void
 ChaiControlBase::execute()
 {
+  chaiscript::ChaiScript chai(chaiscript::Std_Lib::library());
+
+  //chaiscript::ChaiScript chai;
+
+  int result = chai.eval<int>("1 + 3");
+
+  std::cout << "result = " << result << std::endl;
+
+  std::string result2 = chai.eval_file<std::string>("control.chai");
+
+  std::cout << "result2 = " << result2 << std::endl;
 
 
-
-  std::cout << "test" << std::endl;
+//  chaiscript::ChaiScript chai;
+//  chai.add(chaiscript::fun(&callafunc), "callafunc");
+//  chai("callafunc(fun(x) { print(x); })"); // pass a lambda function to the registered function
+                                           // which expects a typed std::function
+//  std::function<void ()> f = chai.eval<std::function<void ()> >("dump_system");
+//  f(); // call the ChaiScript function dump_system, from C++
 
 }
 
