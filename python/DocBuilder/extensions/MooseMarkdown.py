fa799e1a704096a43871e7fe4be00ea324204ec0
Andrew E Slaughter <andrew.slaughter@inl.gov>
2016-05-11 21:58:37 -0600

    Figured out how to get my extension loaded

--- python/DocBuilder/extensions/MooseMarkdown.py
+++ python/DocBuilder/extensions/MooseMarkdown.py
@@ -4,8 +4,8 @@ from markdown.util import etree
 import os
 import re
 
-#MULTI_RE = r'([*/_-]{2})(.*?)\2'
-MULTI_RE = r'\[moose:\s+(.*?):(.*?)\]'
+import clang.cindex
+
 
 MOOSE_DIR = os.getenv('MOOSE_DIR', os.path.join(os.getenv('HOME'), 'projects', 'moose'))
 
@@ -34,15 +34,20 @@ class MooseCpp(object):
 
 class MooseSourcePattern(Pattern):
 
-    #def getCompiledRegExp(self):
-    #    regex = r'\[moose:\s+(.*?)\]'
-    #    return re.compile(regex, flags=re.DOTALL|re.UNICODE)
+    CPP_RE = r'!\[(.*?)\]\((.*\.[Ch])\)'
+
+
+    def __init__(self):
+        Pattern.__init__(self, self.CPP_RE)
 
     def handleMatch(self, match):
+        """
+        Process the C++ file provided.
+        """
 
         # Build the complete filename.
         # NOTE: os.path.join doesn't like the unicode even if you call str() on it first.
-        filename = MOOSE_DIR.rstrip('/') + os.path.sep + match.group(2).lstrip('/')
+        filename = MOOSE_DIR.rstrip('/') + os.path.sep + match.group(3).lstrip('/')
 
         # If the file does not exist return a bold block
         if not os.path.exists(filename):
@@ -60,12 +65,12 @@ class MooseSourcePattern(Pattern):
 class MooseMarkdown(markdown.Extension):
 
     def extendMarkdown(self, md, md_globals):
-        md.inlinePatterns.add('moose_source', MooseSourcePattern(MULTI_RE), '_end')
+        md.inlinePatterns.add('moose_source', MooseSourcePattern(), '_begin')
 
 
-
+def makeExtension(*args, **kwargs):
+    return MooseMarkdown(*args, **kwargs)
 
 if __name__ == '__main__':
-
     md = markdown.Markdown(extensions=[MooseMarkdown()])
-    md.convertFile(input='test.md')
+    md.convertFile(input='/Users/slauae/projects/moose/docs/documentation/moose_style_markdown.md')
