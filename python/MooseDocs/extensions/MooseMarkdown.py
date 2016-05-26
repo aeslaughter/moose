e9c5be5ed519656788a4ffcd7017ed5e0d37bd6f
Andrew E Slaughter <andrew.slaughter@inl.gov>
2016-05-26 14:51:53 -0600

    Working on MOOSE markdown

--- python/MooseDocs/extensions/MooseMarkdown.py
+++ python/MooseDocs/extensions/MooseMarkdown.py
@@ -20,15 +20,14 @@ class MooseCompleteSourcePattern(Pattern):
         Process the C++ file provided.
         """
 
-        print match.groups()
-
         # Build the complete filename.
         # NOTE: os.path.join doesn't like the unicode even if you call str() on it first.
         filename = MooseDocs.MOOSE_DIR.rstrip('/') + os.path.sep + match.group(3).lstrip('/')
 
         # If the file does not exist return a bold block
         if not os.path.exists(filename):
-            el = etree.Element('b')
+            el = etree.Element('font')
+            el.set('color', 'red')
             el.text = 'ERROR: Invalid filename: ' + filename
             return el
 
@@ -37,23 +36,22 @@ class MooseCompleteSourcePattern(Pattern):
         content = fid.read()
         fid.close()
 
+        # Strip header and leading/trailing whitespace and newlines
+        strt = content.find('/********')
+        stop = content.rfind('*******/\n')
+        content = content.replace(content[strt:stop+9], '')
+        content = re.sub(r'^(\n*)', '', content)
+        content = re.sub(r'(\n*)$', '', content)
+
         # Build the Element object
         el = etree.Element('pre')
+        label = etree.SubElement(el, 'p')
+        label.text = match.group(1)
         code = etree.SubElement(el, 'code')
         code.set('class', 'c++')
         code.text = content
         return el
 
-
-
-        """
-        parser = MooseClangParser(filename)
-        el = etree.Element('pre')
-        code = etree.SubElement(el, 'code')
-        code.set('class', 'c++')
-        code.text = parser.method(match.group(3))
-        return el
-        """
 
 class MooseSlideTreeprocessor(Preprocessor):
     def __init(self, *args):
@@ -78,8 +76,6 @@ class MooseMarkdown(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         #md.treeprocessors.add('moose_slides', MooseSlideTreeprocessor(md), '_end')
         md.inlinePatterns.add('moose_complete_source', MooseCompleteSourcePattern(), '_begin')
-
-        print md.inlinePatterns
 
 
 def makeExtension(*args, **kwargs):
