f72dbb7398e9b902781399c1a3aae1f6a0f7eb11
Andrew E Slaughter <andrew.slaughter@inl.gov>
2016-06-03 16:53:18 -0600

    Generator using files from yml config

--- scripts/MkMooseDocs.py
+++ scripts/MkMooseDocs.py
@@ -95,36 +95,39 @@ class MooseSystemInformation(object):
         return '\n'.join(md)
 
 class MooseApplicationDocGenerator(object):
-    def __init__(self, yaml_data, *args):
+    def __init__(self, yaml_data, source):
 
         # Read the supplied files
         self._yaml_data = yaml_data
-        self._objects = []
-        self._systems = []
+        #self._objects = []
+        #self._systems = []
+
+        print source, type(source)
 
 
+        def gen(src, level=0):
+            """
+            Function for looping over supplied source directories.
+            """
+            for key, value in src.iteritems():
+                yield '{}- {}:'.format(' '*4*level, key)
+                if isinstance(value, dict):
+                    out = gen(value, level+1)
+                    yield '\n'.join(out)
+                else:
+                    syntax = MooseDocs.MooseApplicationSyntax(yaml_data, *value)
+                    for k, v in syntax.syntax().iteritems():
+                        out = self._sub(syntax, k, v, level+1)
+                        yield '\n'.join(out)
 
-        output = []
-        for item in args:
-            fname = item[0]
-
-            level = 0
-            for prefix in item[1]:
-                output.append('{}- {}:'.format(' '*4*level, prefix))
-                level += 1
-
-            syntax = MooseDocs.MooseApplicationSyntax(yaml_data, fname)
-
-            for key, value in syntax.syntax().iteritems():
-                output += self._gen(syntax, value, key, level)
-
+        output = gen(source)
         print '\n'.join(output)
 
 
 
 
+    def _sub(self, syntax, key, node, level=0):
 
-    def _gen(self, syntax, node, key, level=0):
 
         ynode = self._yaml_data[node['key']]#['subblocks']
 
@@ -138,7 +141,7 @@ class MooseApplicationDocGenerator(object):
 
         for k, v in node.iteritems():
             if k != 'key':
-                self._gen(syntax, v, k, level+1)
+                self._sub(syntax, v, k, level+1)
 
         if ynode != None:
             if ynode['subblocks'] != None:
@@ -174,14 +177,17 @@ if __name__ == '__main__':
     raw = runExe(exe, '--yaml')
     ydata = utils.MooseYaml(raw)
 
-    #generator = MooseApplicationDocGenerator(ydata, (framework, ['Framework']), (phase_field, ['Modules', 'Phase Field']))
 
 
 
-   # details = MooseDocs.database.Database('.md', include, MooseDocs.database.items.MarkdownIncludeItem)
+    generator = MooseApplicationDocGenerator(ydata, config['extra']['Source'])
 
+
+
+   # Details = MooseDocs.database.Database('.md', include, MooseDocs.database.items.MarkdownIncludeItem)
+    """
     path = '/Kernels/Diffusion'
-
     details = '/Users/slauae/projects/moose-doc/framework/include/kernels/Diffusion.md'
     info = MooseDocs.MooseObjectInformation(ydata[path], details, inputs=inputs, source=source)
     info.write()
+    """
