46f82f42bb71ae8aefcc2b8c4485434d52a0861d
Andrew E Slaughter <andrew.slaughter@inl.gov>
2016-10-25 21:36:02 -0600

    toc working

--- docs/js/init.js
+++ docs/js/init.js
@@ -3,6 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.toc-wrapper').pushpin({
+        top: 64
+    });
+    $('.scrollspy').scrollSpy();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
