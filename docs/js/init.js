aa9cc53f677eb66163f47a4e5ce1652a3f40ff57
Andrew E Slaughter <andrew.slaughter@inl.gov>
2016-10-25 20:20:08 -0600

    Working navigation on index.html

--- docs/js/init.js
+++ docs/js/init.js
@@ -1,3 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
 // Setup MathJax
 MathJax.Hub.Config({
     tex2jax: {
@@ -5,25 +14,3 @@ MathJax.Hub.Config({
         inlineMath: [['$','$'], ['\\(','\\)']]
     }
 });
-
-// Materialize side navigation
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav();
-
-      // Show sideNav
-      $('.button-collapse').sideNav('show');
-      // Hide sideNav
-      $('.button-collapse').sideNav('hide');
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-// Materialize scrolling table-of-contents
-//$(document).ready(function(){
-//    $('.scrollspy').scrollSpy();
-//});
-
-
-//$("h1","h2").addClass("section scollSpy");
-vb
