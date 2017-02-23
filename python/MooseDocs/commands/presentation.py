2672cf5e40b9675d747b81b1046ed52b757ba7c3
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-02-23 14:09:58 -0700

    Re-factored 'nodes' to allow for presentation to utilize similar code

--- python/MooseDocs/commands/presentation.py
+++ python/MooseDocs/commands/presentation.py
@@ -2,7 +2,7 @@ import os
 import jinja2
 import markdown
 import MooseDocs
-from MooseDocsMarkdownNode import MooseDocsMarkdownNode
+from MooseDocsMarkdownNodeBase import MooseDocsMarkdownNodeBase
 
 def presentation_options(parser):
     """
@@ -10,7 +10,7 @@ def presentation_options(parser):
     """
     reveal_default = os.path.join(os.getenv('HOME'), 'projects', 'reveal.js')
 
-    parser.add_argument('input', type=str, help="The markdown file to convert to slides.")
+    parser.add_argument('md_file', type=str, help="The markdown file to convert to slides.")
     parser.add_argument('--output', '-o', default=None, type=str, help="The default html file to create, defaults to input filename with html extension.")
     parser.add_argument('--template', type=str, default='presentation.html', help="The template html file to utilize (default: %(default)s).")
     parser.add_argument('--title', type=str, default="MOOSE Presentation", help="The title of the document.")
@@ -18,15 +18,26 @@ def presentation_options(parser):
     parser.add_argument('--scripts', type=str, nargs='+', default=[os.path.join(MooseDocs.MOOSE_DIR, 'docs', 'js', 'init.js')], help="A list of additional js files to inject into the presentation html file (%(default)s).")
     return presentation
 
-def insert_file(filename):
-    """
-    Helper function for jinja2 to read css file and return as string.
-    """
-    with open(filename, 'r') as fid:
-        return fid.read().strip('\n')
-    return ''
+class PresentationBuilder(MooseDocsMarkdownNodeBase):
 
-def presentation(config_file='moosedocs.yml', input=None, output=None, template=None, **template_args):
+    def environment(self):
+        """
+        Override base to add file insert filter.
+        """
+        env = super(PresentationBuilder, self).environment()
+        env.globals['insert_file'] = self.insertFile
+        return env
+
+    @staticmethod
+    def insertFile(filename):
+        """
+        Helper function for jinja2 to read css file and return as string.
+        """
+        with open(filename, 'r') as fid:
+            return fid.read().strip('\n')
+            return ''
+
+def presentation(config_file='moosedocs.yml', md_file=None, output=None, template=None, **template_args):
     """
     MOOSE markdown presentation blaster.
     """
@@ -40,23 +51,6 @@ def presentation(config_file='moosedocs.yml', input=None, output=None, template=
     extensions.append('MooseDocs.extensions.MooseMarkdownPresentation')
     parser = markdown.Markdown(extensions=extensions, extension_configs=extension_configs)
 
-    # Create the jinja2 template
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader([os.path.join(MooseDocs.MOOSE_DIR, 'docs', 'templates'),
-                                                             os.path.join(os.getcwd(), 'templates')]))
-    env.globals['insert_file'] = insert_file
-    template = env.get_template(template)
-
-    # Read the markdown and convert to html
-    with open(input, 'r') as fid:
-        md = fid.read()
-    html = parser.convert(md)
-
-    # Inject slides into presentation template
-    complete = template.render(content=html, **config['template_arguments'])
-
-    # Write the output file
-    if not output:
-        name, _ = os.path.splitext(input)
-        output = name + '.html'
-    with open(output, 'w') as fid:
-        fid.write(complete)
+    site_dir, _ = os.path.splitext(md_file)
+    root = PresentationBuilder(name='', md_file=md_file, parser=parser, template=template, template_args=template_args, site_dir=site_dir)
+    root.build()
--- python/MooseDocs/extensions/MooseInlineProcessor.py
+++ python/MooseDocs/extensions/MooseInlineProcessor.py
@@ -1,23 +1,37 @@
-from markdown.treeprocessors import InlineProcessor
 import logging
 log = logging.getLogger(__name__)
-
+from markdown.treeprocessors import InlineProcessor
 from MooseImageFile import MooseImageFile
 from MooseCommonExtension import MooseCommonExtension
 
 class MooseInlineProcessor(InlineProcessor, MooseCommonExtension):
     """
-    Replacement for the standard InlineProcessor that includes an initialize() method.
+    Replacement for the standard InlineProcessor that includes special features for MooseMarkdown.
+        1. Calls an initialize() method on all inline patterns before processing the tree
+        2. Adds a class "moose-list" to the top-level of lists.
     """
-
-    def __init__(self, markdown_instance=None, **kwargs):
+    def __init__(self, markdown_instance=None, **kargs):
         super(MooseInlineProcessor, self).__init__(markdown_instance)
 
-    def run(self, *args, **kwargs):
+    def run(self, root, **kwargs):
         """
         Adds a call to 'initialize()' prior to executing the InlineProcessor running.
         """
+
+        # Call initialize method (see MooseFigure)
         for inline in self.inlinePatterns.itervalues():
             if hasattr(inline, 'initialize'):
                 inline.initialize()
-        super(MooseInlineProcessor, self).run(*args, **kwargs)
+
+        # Set the class name for top-level ul, ol tags (Allows for lists to be made into columns for slides)
+        items = root.findall('ul') + root.findall('ol')
+        for it in root.findall('ul') + root.findall('ol'):
+            for c in it.iterfind('ul'):
+                items.remove(c)
+            for c in it.iterfind('ol'):
+                items.remove(c)
+
+        for it in items:
+            it.set('class', 'moose-list')
+
+        super(MooseInlineProcessor, self).run(root, **kwargs)
