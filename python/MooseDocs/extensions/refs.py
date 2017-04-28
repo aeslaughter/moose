1e983f45c29a5b2c333e66a00d6e5473819bf074
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-04-27 21:37:34 -0600

    refs

--- python/MooseDocs/extensions/refs.py
+++ python/MooseDocs/extensions/refs.py
@@ -1,26 +1,36 @@
-import os
-import re
+#pylint: disable=missing-docstring
+#################################################################
+#                   DO NOT MODIFY THIS HEADER                   #
+#  MOOSE - Multiphysics Object Oriented Simulation Environment  #
+#                                                               #
+#            (c) 2010 Battelle Energy Alliance, LLC             #
+#                      ALL RIGHTS RESERVED                      #
+#                                                               #
+#           Prepared by Battelle Energy Alliance, LLC           #
+#             Under Contract No. DE-AC07-05ID14517              #
+#              With the U. S. Department of Energy              #
+#                                                               #
+#              See COPYRIGHT for full restrictions              #
+#################################################################
 import collections
-import bs4
 import logging
-log = logging.getLogger(__name__)
+import bs4
 
-import markdown
 from markdown.util import etree
 from markdown.inlinepatterns import Pattern
 from markdown.postprocessors import Postprocessor
 
-import MooseDocs
 from MooseMarkdownExtension import MooseMarkdownExtension
 from MooseMarkdownCommon import MooseMarkdownCommon
 
+LOG = logging.getLogger(__name__)
 
 class RefExtension(MooseMarkdownExtension):
     """
-    Adds \ref and \eqref support.
+    Adds ref and eqref support.
 
-    \eqref: works with MathJax equation reference support.
-    \ref: works with captions create with MooseDocs (see tables.py, media.py).
+    eqref: works with MathJax equation reference support.
+    ref: works with captions create with MooseDocs (see tables.py, media.py, and listings.py).
     """
 
     @staticmethod
@@ -30,12 +40,14 @@ class RefExtension(MooseMarkdownExtension):
 
     def extendMarkdown(self, md, md_globals):
         """
-        Adds \eqref support for MOOSE flavored markdown.
+        Adds eqref support for MOOSE flavored markdown.
         """
         md.registerExtension(self)
         config = self.getConfigs()
 
-        md.inlinePatterns.add('moose-eq-ref', EquationPattern(markdown_instance=md, **config), '_begin')
+        md.inlinePatterns.add('moose-eq-ref',
+                              EquationPattern(markdown_instance=md, **config),
+                              '_begin')
 
         ref = FloatReferencePattern(markdown_instance=md, **config)
         md.inlinePatterns.add('moose-ref', ref, '_end')
@@ -49,14 +61,17 @@ def makeExtension(*args, **kwargs): #pylint: disable=invalid-name
 
 class FloatReferencePattern(Pattern):
     """
-    Creates a <span> tag for unknown \ref commands.
+    Creates a <span> tag for unknown ref commands.
     """
     RE = r'(?<![`])(\\ref{(.*?)})'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs): #pylint: disable=unused-argument
         Pattern.__init__(self, self.RE, *args)
 
     def handleMatch(self, match):
+        """
+        Creates an <a> tag for storing the reference.
+        """
         el = etree.Element('a')
         el.text = match.group(2)
         el.set('class', 'moose-unknown-reference')
@@ -65,9 +80,9 @@ class FloatReferencePattern(Pattern):
 
 class EquationPattern(MooseMarkdownCommon, Pattern):
     """
-    Defines syntax for referencing MathJax equations with \label defined.
+    Defines syntax for referencing MathJax equations with label defined.
 
-    This should be handled automatically by MathJax, but I can't seem to get the \eqref stuff
+    This should be handled automatically by MathJax, but I can't seem to get the eqref stuff
     working via MathJax. I am guessing that the python-markdown-math package is doing something to
     break compatibility. I also can't get latex math to work without the package, so until I have
     more time to dig I am just building the references manually.
@@ -81,7 +96,7 @@ class EquationPattern(MooseMarkdownCommon, Pattern):
 
     def handleMatch(self, match):
         """
-        Creates the <a> object with the reference that is then updated with the function in the init.js file.
+        Creates the <a> object with the reference that is updated with FloatLinker.
         """
         mjx_id = 'mjx-eqn-{}'.format(match.group(2).replace(':', ''))
         el = etree.Element('a')
@@ -92,11 +107,17 @@ class EquationPattern(MooseMarkdownCommon, Pattern):
 
 
 class FloatLinker(Postprocessor):
+    """
+    Numbers floats in order on a page and updates the ref commands to the assigned numbers.
+    """
 
-    def __init__(self, markdown_instance=None, **kwargs):
+    def __init__(self, markdown_instance=None, **kwargs): #pylint: disable=unused-argument
         super(FloatLinker, self).__init__(markdown_instance)
 
     def run(self, text):
+        """
+        Parsers the text and creates float numbers and updates the ref commands.
+        """
 
         soup = bs4.BeautifulSoup(text, 'html.parser')
         lookup = dict()
