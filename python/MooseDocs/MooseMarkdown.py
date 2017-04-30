a91022e055570abf79fbbb664b52b79e8e472ef6
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-04-29 20:11:55 -0600

    pylint

--- python/MooseDocs/MooseMarkdown.py
+++ python/MooseDocs/MooseMarkdown.py
@@ -1,36 +1,56 @@
-import os
-import markdown
-import collections
+#pylint: disable=missing-docstring
+#################################################################
+#                   DO NOT MODIFY THIS HEADER                   #
+#  MOOSE - Multiphysics Object Oriented Simulation Environment  #
+#                                                               #
+#            (c) 2010 Battelle Energy Alliance, LLC             #
+#                      ALL RIGHTS RESERVED                      #
+#                                                               #
+#           Prepared by Battelle Energy Alliance, LLC           #
+#             Under Contract No. DE-AC07-05ID14517              #
+#              With the U. S. Department of Energy              #
+#                                                               #
+#              See COPYRIGHT for full restrictions              #
+#################################################################
 import logging
-log = logging.getLogger(__name__)
+
+import markdown
 
 import mooseutils
-import MooseDocs
 from MooseDocs.commands.MarkdownNode import MarkdownNode
-from MooseDocs.extensions.media import MediaPatternBase
+
+LOG = logging.getLogger(__name__)
 
 class MooseMarkdown(markdown.Markdown):
     """
     A custom Markdown object for handling raw text, markdown files, or MarkdownNode objects.
 
-    Additionally, this class automatically loads the required markdown extensions.
-
-    The key to this class is allowing the Markdown object to work with MarkdownNode objects such that the
-    extension objects, namely MooseTemplate, could have access to the node object to allow for searching the tree
-    for other pages. This should allow for cross page figure, equation, and table links to be created.
+    The key to this class is allowing the Markdown object to work with MarkdownNode objects such
+    that the extension objects, namely MooseTemplate, could have access to the node object to allow
+    for searching the tree for other pages. This should allow for cross page figure, equation, and
+    table links to be created.
     """
-    CODE_BLOCK_COUNT = 0
+    CODE_BLOCK_COUNT = 0 # counter for code block copy buttons
 
-    def __init__(self, extensions=[], extension_configs=dict()):
+    def __init__(self, extensions=None, extension_configs=None):
+
+        # Initialize member variables
         self.current = None # member for holding the current MarkdownNode object
-        super(MooseMarkdown, self).__init__(extensions=extensions, extension_configs=extension_configs)
+        if extensions is None:
+            extensions = []
+        if extension_configs is None:
+            extension_configs = dict()
+
+        super(MooseMarkdown, self).__init__(extensions=extensions,
+                                            extension_configs=extension_configs)
 
     def requireExtension(self, required):
         """
         Raise an exception of the supplied extension type is not registered.
         """
         if not self.getExtension(required):
-            raise mooseutils.MooseException("The {} extension is required.".format(required.__name__))
+            raise mooseutils.MooseException("The {} extension is required." \
+                                            .format(required.__name__))
 
     def getExtension(self, etype):
         """
