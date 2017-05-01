1a83f09f80580bde73ff231a178ba5c3dd4d6cac
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-05-01 13:39:41 -0600

    commands clean

--- python/MooseDocs/commands/__init__.py
+++ python/MooseDocs/commands/__init__.py
@@ -1,3 +1,18 @@
+#pylint: disable=missing-docstring
+#################################################################
+#                   DO NOT MODIFY THIS HEADER                   #
+#  MOOSE - Multiphysics Object Oriented Simulation Environment  #
+#                                                               #
+#            (c) 2010 Battelle Energy Alliance, LLC             #
+#                      ALL RIGHTS RESERVED                      #
+#                                                               #
+#           Prepared by Battelle Energy Alliance, LLC           #
+#             Under Contract No. DE-AC07-05ID14517              #
+#              With the U. S. Department of Energy              #
+#                                                               #
+#              See COPYRIGHT for full restrictions              #
+#################################################################
+
 from check import check, check_options
 from build import build, build_options
 from latex import latex, latex_options
