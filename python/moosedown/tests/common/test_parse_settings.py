031f0c42a0716b83d92b78eb0698d7b800101e2d
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-10-17 16:25:52 -0600

    add testing

--- python/moosedown/tests/common/test_parse_settings.py
+++ python/moosedown/tests/common/test_parse_settings.py
@@ -17,5 +17,26 @@ class TestParseSettings(unittest.TestCase):
         self.assertEqual(known['day'], 24)
         self.assertEqual(unknown, dict())
 
+    def testSpace(self):
+        defaults = dict(year=(1980, 'doc'), month=('june', 'doc'), day=(24, 'doc'))
+        raw = 'year=the year I was born'
+        known, _ = common.parse_settings(defaults, raw)
+        self.assertEqual(known['year'], 'the year I was born')
+
+    def testFloat(self):
+        defaults = dict(year=(1980, 'doc'))
+        raw = 'year=2003'
+        known, _ = common.parse_settings(defaults, raw)
+        self.assertIsInstance(known['year'], float)
+
+    def testUnknown(self):
+        defaults = dict(year=(1980, 'doc'))
+        raw = 'year=2003 month=june'
+        known, unknown = common.parse_settings(defaults, raw)
+        self.assertEqual(known['year'], 2003)
+        self.assertNotIn('month', known)
+        self.assertIn('month', unknown)
+        self.assertEqual(unknown['month'], 'june')
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
