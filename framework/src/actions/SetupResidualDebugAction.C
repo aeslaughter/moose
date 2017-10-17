ea99c9152595a507d5069f1c76636130c46c6aef
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-10-17 17:14:02 -0600

    Enables arbitrary execute flags
    (closes #7489)

--- framework/src/actions/SetupResidualDebugAction.C
+++ framework/src/actions/SetupResidualDebugAction.C
@@ -69,7 +69,8 @@ SetupResidualDebugAction::act()
     InputParameters params = _factory.getValidParams("DebugResidualAux");
     params.set<AuxVariableName>("variable") = aux_var_name;
     params.set<NonlinearVariableName>("debug_variable") = var.name();
-    params.set<MultiMooseEnum>("execute_on") = "linear timestep_end";
+    params.set<ExecFlagEnum>("execute_on") =
+        std::vector<ExecFlagType>({EXEC_LINEAR, EXEC_TIMESTEP_END});
     _problem->addAuxKernel("DebugResidualAux", kern_name, params);
   }
 }
