16a4e51a8e7ead9d8bcc8149c7348975bd93808f
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-10-17 19:19:17 -0600

    foo

--- python/moosedown/tests/common/test_parse_settings.py
+++ python/moosedown/tests/common/test_parse_settings.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+"""Testing for common.parse_settings function."""
+####################################################################################################
+#                                    DO NOT MODIFY THIS HEADER                                     #
+#                   MOOSE - Multiphysics Object Oriented Simulation Environment                    #
+#                                                                                                  #
+#                              (c) 2010 Battelle Energy Alliance, LLC                              #
+#                                       ALL RIGHTS RESERVED                                        #
+#                                                                                                  #
+#                            Prepared by Battelle Energy Alliance, LLC                             #
+#                               Under Contract No. DE-AC07-05ID14517                               #
+#                               With the U. S. Department of Energy                                #
+#                                                                                                  #
+#                               See COPYRIGHT for full restrictions                                #
+####################################################################################################
+
 import unittest
 from MooseDocs import common
 
@@ -8,6 +23,7 @@ class TestParseSettings(unittest.TestCase):
     """
     def testBasic(self):
         """
+        Test setting a value.
         """
         defaults = dict(year=(1980, 'doc'), month=('june', 'doc'), day=(24, 'doc'))
         raw = 'year=2003'
@@ -18,18 +34,27 @@ class TestParseSettings(unittest.TestCase):
         self.assertEqual(unknown, dict())
 
     def testSpace(self):
+        """
+        Test that values can have spaces.
+        """
         defaults = dict(year=(1980, 'doc'), month=('june', 'doc'), day=(24, 'doc'))
         raw = 'year=the year I was born'
         known, _ = common.parse_settings(defaults, raw)
         self.assertEqual(known['year'], 'the year I was born')
 
     def testFloat(self):
+        """
+        Test float conversion.
+        """
         defaults = dict(year=(1980, 'doc'))
         raw = 'year=2003'
         known, _ = common.parse_settings(defaults, raw)
         self.assertIsInstance(known['year'], float)
 
     def testUnknown(self):
+        """
+        Test unknown key, value pairs.
+        """
         defaults = dict(year=(1980, 'doc'))
         raw = 'year=2003 month=june'
         known, unknown = common.parse_settings(defaults, raw)
