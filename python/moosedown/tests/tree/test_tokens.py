8e7a64cd988bda76a9c60ca4f4755b27f14bf57a
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-10-23 17:21:22 -0600

    Working on descriptors

--- python/moosedown/tests/tree/test_tokens.py
+++ python/moosedown/tests/tree/test_tokens.py
@@ -9,7 +9,7 @@ class TestTokens(unittest.TestCase):
 
     @mock.patch('logging.Logger.error')
     def testToken(self, mock):
-        token = tokens.Token(None)
+        token = tokens.Token()
         self.assertEqual(token.name, 'Token')
         self.assertEqual(token.line, None)
         token.line = 42
@@ -21,42 +21,96 @@ class TestTokens(unittest.TestCase):
         self.assertIn('must be of type "int"', args[0])
 
     def testString(self):
-        token = tokens.String(None, content="content")
+        token = tokens.String(content="content")
         self.assertEqual(token.content, "content")
 
+        with self.assertRaises(TypeError) as e:
+            token = tokens.String(content=1980)
+        self.assertIn('The content must be a str', e.exception.message)
+
     def testUnknown(self):
-        token = tokens.Unknown(None, content="content")
+        token = tokens.Unknown(content="content")
         self.assertEqual(token.content, "content")
 
     def testWord(self):
-        token = tokens.Word(None, content="content")
+        token = tokens.Word(content="content")
         self.assertEqual(token.content, "content")
 
     def testSpace(self):
-        token = tokens.Space(None)
+        token = tokens.Space()
         self.assertEqual(token.content, ' ')
         self.assertEqual(token.count, 1)
 
-        token = tokens.Space(None, count=42)
+        token = tokens.Space(count=42)
         self.assertEqual(token.content, ' ')
         self.assertEqual(token.count, 42)
 
         with self.assertRaises(TypeError) as e:
-            token = tokens.Space(None, count='not int')
+            token = tokens.Space(count='not int')
         self.assertIn('The count must be an int', e.exception.message)
 
     def testBreak(self):
-        token = tokens.Break(None)
+        token = tokens.Break()
         self.assertEqual(token.content, '\n')
         self.assertEqual(token.count, 1)
 
-        token = tokens.Break(None, count=42)
+        token = tokens.Break(count=42)
         self.assertEqual(token.content, '\n')
         self.assertEqual(token.count, 42)
 
         with self.assertRaises(TypeError) as e:
-            token = tokens.Space(None, count='not int')
+            token = tokens.Space(count='not int')
         self.assertIn('The count must be an int', e.exception.message)
+
+    def testPunctuation(self):
+        token = tokens.Punctuation(content='---')
+        self.assertEqual(token.content, '---')
+
+    def testNumber(self):
+        token = tokens.Punctuation(content='1980')
+        self.assertEqual(token.content, '1980')
+
+    def testCode(self):
+        token = tokens.Code(content="x+y=2;")
+        self.assertEqual(token.content, "x+y=2;")
+
+    def testHeading(self):
+        token = tokens.Heading(level=4)
+        self.assertEqual(token.level, 4)
+
+        with self.assertRaises(TypeError) as e:
+            token = tokens.Heading(level='not int')
+        self.assertIn('The level must be an int', e.exception.message)
+
+    def testParagraph(self):
+        token = tokens.Paragraph()
+
+    def testUnorderedList(self):
+        token = tokens.UnorderedList()
+
+    def testOrderedList(self):
+        token = tokens.OrderedList()
+        self.assertEqual(token.start, 1)
+        token = tokens.OrderedList(start=1980)
+        self.assertEqual(token.start, 1980)
+
+        with self.assertRaises(TypeError) as e:
+            token = tokens.OrderedList(start='not int')
+        self.assertIn('The start must be an int', e.exception.message)
+
+    def testListItem(self):
+        with self.assertRaises(IOError) as e:
+            token = tokens.ListItem()
+        self.assertIn("A 'ListItem' must have a 'OrderedList' or 'UnorderedList' parent.",
+                      e.exception.message)
+
+        root = tokens.OrderedList()
+        token = tokens.ListItem(parent=root)
+        self.assertIs(token.parent, root)
+
+    def testLink(self):
+        pass
+
 
 
 if __name__ == '__main__':
