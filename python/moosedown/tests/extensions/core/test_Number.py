fa3fc21ab8599089cebe796b97f4b8a59f9fe481
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-17 16:36:26 -0700

    more

---
+++ python/moosedown/tests/extensions/core/test_Number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import unittest
+import logging
+import mock
+
+from moosedown import tree
+from moosedown.base import testing
+
+class TestNumberTokenize(testing.MarkdownTestCase):
+    def testBasic(self):
+        node = self.ast('foo1bar')(0)
+        self.assertIsInstance(node(0), tree.tokens.Word)
+        self.assertIsInstance(node(1), tree.tokens.Number)
+        self.assertIsInstance(node(2), tree.tokens.Word)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
