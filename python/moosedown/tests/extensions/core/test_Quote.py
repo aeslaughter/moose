e57061ead8e5dc719bcd250df816ce372937ecaf
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-27 14:30:17 -0700

    More tests

---
+++ python/moosedown/tests/extensions/core/test_Quote.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import unittest
+
+from moosedown import tree
+from moosedown.base import testing
+
+class TestQuoteTokenize(testing.MarkdownTestCase):
+    def testBasic(self):
+        node = self.ast('> foo bar')(0)
+        Self.assertIsInstance(node, tree.tokens.Quote)
+        self.assertIsInstance(node(0), tree.tokens.Paragraph)
+        self.assertIsInstance(node(0)(0), tree.tokens.Word)
+        self.assertIsInstance(node(0)(1), tree.tokens.Space)
+        self.assertIsInstance(node(0)(2), tree.tokens.Word)
+
+        self.assertString(node(0)(0).content, 'foo')
+        self.assertString(node(0)(1).content, ' ')
+        self.assertString(node(0)(2).content, 'bar')
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
