2cc5de19484bc84d9719b454ec228d6686f087cf
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-28 14:04:20 -0700

    Alternate method for auto setting execute flag id
    (refs #7489)

--- framework/src/base/MooseApp.C
+++ framework/src/base/MooseApp.C
@@ -1347,7 +1347,5 @@ MooseApp::registerExecFlags()
 void
 MooseApp::registerExecFlag(const ExecFlagType & flag)
 {
-  if (flag.id() == MooseEnumItem::INVALID_ID)
-    flag._id = Moose::execute_flags.getNextValidID();
   Moose::execute_flags.addAvailableFlags(flag);
 }
