e5848eac09a054dd825efb4c13b28539582bf0d4
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-29 16:17:29 -0700

    Working on migrate md files tool

--- python/moosedown/mv.py
+++ python/moosedown/mv.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import logging
 
 import MooseDocs
@@ -8,13 +9,6 @@ from MooseDocs.extensions.app_syntax import AppSyntaxExtension
 app = 'framework'
 source_root = '/Users/slauae/projects/moosedown/docs/content/documentation/systems'
 
-if app == 'framework':
-    destination_root = '/Users/slauae/projects/moosedown/{}/doc/content/documentation/systems'
-else:
-    destination_root = '/Users/slauae/projects/moosedown/modules/{}/doc/content/documentation/systems'
-destination_root = destination_root.format(app)
-
-
 logging.basicConfig()
 config = MooseDocs.load_config('/Users/slauae/projects/moosedown/docs/website.yml')
 parser = MooseMarkdown(config)
@@ -22,4 +16,16 @@ ext = parser.getExtension(AppSyntaxExtension)
 syntax = ext.getMooseAppSyntax()
 
 
-print syntax
+for node in syntax.findall():
+    group = node.groups.keys()[0]
+    if group == 'framework':
+        destination_root = '/Users/slauae/projects/moosedown/{}/doc/content/documentation/systems'
+    else:
+        destination_root = '/Users/slauae/projects/moosedown/modules/{}/doc/content/documentation/systems'
+
+    src = node.markdown(source_root)
+    dst = node.markdown('', absolute=False).replace('/{}/'.format(group), '/')
+    dst = os.path.join(destination_root.format(group), dst)
+    cmd = ['git', 'mv', src, dst]
+    print ' '.join(cmd)
+    #subprocess.call(cmd)
