b933dc50cf5a0e5545827e374ee94ca4b6c7e8a2
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-29 09:14:55 -0700

    list item

---
+++ python/moosedown/tests/extensions/core/test_RenderListItem.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import unittest
+import logging
+import mock
+
+from moosedown import tree
+from moosedown.base import testing, MaterializeRenderer, LatexRenderer
+
+class TestRenderListItemHTML(testing.MarkdownTestCase):
+
+    def testWrite(self):
+        node = tree.tokens.UnorderedList(None)
+        tree.tokens.ListItem(node)
+        html = self.write(self.render(node).find('body')(-1))
+        self.assertEqual(html, '<ul><li></li></ul>')
+
+    def testError(self):
+        with self.assertRaises(IOError) as e:
+            tree.tokens.ListItem(None)
+        self.assertIn("A 'ListItem' must have a 'OrderedList' or 'UnorderedList' parent.",
+                      str(e.exception))
+
+
+class TestRenderListItemMaterialize(TestRenderListItemHTML):
+    RENDERER = MaterializeRenderer
+    def testWrite(self):
+        node = tree.tokens.UnorderedList(None)
+        tree.tokens.ListItem(node)
+        html = self.write(self.render(node).find('body')(0)(0))
+        self.assertEqual(html, '<ul class="browser-default"><li></li></ul>')
+
+class TestRenderListItemLatex(testing.MarkdownTestCase):
+    RENDERER = LatexRenderer
+
+    def testWrite(self):
+        ast = tree.tokens.UnorderedList(None)
+        tree.tokens.ListItem(ast)
+        node = self.render(ast).find('document')(-1)
+        tex = self.write(node).strip('\n')
+        self.assertString(tex, '\\begin{itemize}\n\\item\n\\end{itemize}')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
