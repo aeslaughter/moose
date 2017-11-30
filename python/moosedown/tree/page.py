013addf693b4ad507fad59fed38645657528f6b8
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-11-30 14:07:16 -0700

    testing

--- python/moosedown/tree/page.py
+++ python/moosedown/tree/page.py
@@ -10,29 +10,28 @@ import mooseutils
 
 LOG = logging.getLogger(__name__)
 
-class FileTreeNodeBase(base.NodeBase):
+class PageNodeBase(base.NodeBase):
     COLOR = None
     PROPERTIES = [base.Property('source', ptype=str)]
 
     @property
     def local(self):
-        path = os.path.join(self.parent.local, self.name) if self.parent else ''
+        path = os.path.join(self.parent.local, self.name) if self.parent else self.name
         return path
 
     def __repr__(self):
         out = '{} ({}): {}'.format(self.name, self.__class__.__name__, self.source)
         return mooseutils.colorText(out, self.COLOR)
 
-class DirectoryNode(FileTreeNodeBase):
+class DirectoryNode(PageNodeBase):
     COLOR = 'CYAN'
 
-
-class FileNode(FileTreeNodeBase):
-    PROPERTIES = FileTreeNodeBase.PROPERTIES + [base.Property('content', ptype=unicode)]
+class FileNode(PageNodeBase):
+    PROPERTIES = PageNodeBase.PROPERTIES + [base.Property('content', ptype=unicode)]
     COLOR = 'MAGENTA'
 
     def __init__(self, *args, **kwargs):
-        base.NodeBase.__init__(self, *args, **kwargs)
+        PageNodeBase.__init__(self, *args, **kwargs)
 
         if os.path.exists(self.source):
             with codecs.open(self.source, encoding='utf-8') as fid:
