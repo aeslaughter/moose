4f443d1eb967142d444464bd8a85a8bdc6494259
Andrew E Slaughter <andrew.slaughter@inl.gov>
2017-12-21 14:37:58 -0700

    working on building master

--- python/moosedown/extensions/include.py
+++ python/moosedown/extensions/include.py
@@ -31,10 +31,11 @@ class Include(core.MarkdownCommandComponent):
         node = self.reader.node.findall(match.group('filename'), maxcount=1)[0]
         node.read()
 
+        #TODO: check type of node, must be markdown for this to work
+
         # Set the master node, so that when livereload fires on this included node that the
         # node that does the including is reloaded
         #print node.master.name, self.reader.node.name
-        #print self.reader.node.name, 'adding to master of', node.name
         node.master.add(self.reader.node)
 
         ast = self.translator.ast(node)
