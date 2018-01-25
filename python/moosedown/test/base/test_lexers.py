00c6130988096c0019322031475f9d839313405d
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-01-25 14:38:09 -0700

    WIP

--- python/moosedown/test/base/test_lexers.py
+++ python/moosedown/test/base/test_lexers.py
@@ -2,14 +2,26 @@
 """
 Tests for Lexer objects.
 """
+import re
 import unittest
+from moosedown.base import lexers
 
 class TestLexerInformation(unittest.TestCase):
     """
     Test Lexer base class.
     """
     def test(self):
-        pass
+        text = 'foo bar'
+        regex = re.compile(r'foo')
+        match = regex.search(text)
+        def func():
+            pass
+
+        print dir(lexers)
+
+        pattern = lexers.Grammer.Pattern(name='name', regex=regex, function=func)
+        info = lexers.LexerInformation(match=match, pattern=pattern, line=42)
+        print info
 
 
 if __name__ == '__main__':
