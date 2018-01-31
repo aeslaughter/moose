930331148eee08b945df5e77bc03a7309d97aeef
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-01-31 11:11:15 -0700

    keep going, pylint later

--- python/moosedown/base/__init__.py
+++ python/moosedown/base/__init__.py
@@ -1,5 +1,10 @@
+"""
+The base module defines the primary base classes for creating MooseDocs objects for
+converting Markdown into HTML or LaTeX.
+"""
 from lexers import Lexer, RecursiveLexer
 from readers import Reader, MarkdownReader
 from renderers import Renderer, HTMLRenderer, MaterializeRenderer, LatexRenderer
 from translators import Translator
-from components import Extension, RenderComponent, TokenComponent#TODO: TokenComponent -> ReaderComponent
+from components import Extension, RenderComponent, TokenComponent
+#TODO: TokenComponent -> ReaderComponent
