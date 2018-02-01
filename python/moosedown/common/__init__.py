fb53128639402494e7469318cd2b12ec2e3305ca
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-01-31 18:12:04 -0700

    common pylint clean and tested

--- python/moosedown/common/__init__.py
+++ python/moosedown/common/__init__.py
@@ -1,3 +1,6 @@
+"""
+Module for objects and functions that are commonly used throughout the MooseDocs system.
+"""
 from storage import Storage
 from check_type import check_type
 from parse_settings import parse_settings
