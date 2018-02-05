8723bcbb4da6306b6331ac709357aa7802f30c7c
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-02-05 13:54:06 -0700

    more

--- python/moosedown/test/extensions/core/test_RenderListItem.py
+++ python/moosedown/test/extensions/core/test_RenderListItem.py
@@ -7,12 +7,14 @@ from moosedown import tree
 from moosedown.base import testing, MaterializeRenderer, LatexRenderer
 
 class TestRenderListItemHTML(testing.MooseDocsTestCase):
-
-    def testWrite(self):
+    def node(self):
         node = tree.tokens.UnorderedList(None)
         tree.tokens.ListItem(node)
-        html = self.write(self.render(node).find('body')(-1))
-        self.assertEqual(html, '<ul><li></li></ul>')
+        return self._renderer.render(node).find('body')
+
+    def testWrite(self):
+        html = self.node()(0)
+        self.assertEqual(html.write(), '<ul><li></li></ul>')
 
     def testError(self):
         with self.assertRaises(IOError) as e:
@@ -20,25 +22,20 @@ class TestRenderListItemHTML(testing.MooseDocsTestCase):
         self.assertIn("A 'ListItem' must have a 'OrderedList' or 'UnorderedList' parent.",
                       str(e.exception))
 
-
 class TestRenderListItemMaterialize(TestRenderListItemHTML):
     RENDERER = MaterializeRenderer
     def testWrite(self):
-        node = tree.tokens.UnorderedList(None)
-        tree.tokens.ListItem(node)
-        html = self.write(self.render(node).find('body')(0)(0))
-        self.assertEqual(html, '<ul class="browser-default"><li></li></ul>')
+        html = self.node()(0)(0)(0)(0)
+        self.assertEqual(html.write(), '<ul class="browser-default"><li></li></ul>')
 
 class TestRenderListItemLatex(testing.MooseDocsTestCase):
     RENDERER = LatexRenderer
 
     def testWrite(self):
-        ast = tree.tokens.UnorderedList(None)
-        tree.tokens.ListItem(ast)
-        node = self.render(ast).find('document')(-1)
-        tex = self.write(node).strip('\n')
-        self.assertString(tex, '\\begin{itemize}\n\\item\n\\end{itemize}')
-
+        node = tree.tokens.UnorderedList(None)
+        tree.tokens.ListItem(node)
+        tex = self._renderer.render(node).find('document')(0)
+        self.assertString(tex.write(), '\n\\begin{itemize}\n\n\\item\n\\end{itemize}\n')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
