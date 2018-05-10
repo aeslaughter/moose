06f4c53238b26f48ef2927c9ed8ff43c995b963b
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-05-10 17:24:37 -0600

    using class

--- python/chigger/vtk_scripts/time_interpolator.py
+++ python/chigger/vtk_scripts/time_interpolator.py
@@ -2,23 +2,32 @@
 import os
 import vtk
 
+from vtk.util.vtkAlgorithm import VTKPythonAlgorithmBase
+
+"""
 class TimeSeries(object):
 
     def Initialize(self, vtkself):
         vtkself.SetNumberOfInputPorts(1)
         vtkself.SetNumberOfOutputPorts(1)
 
-
-
-
     def ProcessRequest(self, vtkself, request, inInfo, outInfo):
-        if request.Has(vtk.vtkDemandDrivenPipeline.REQUEST_DATA()):
+
+        if request.Has(vtk.vtkDemandDrivenPipeline.REQUEST_
+
+
+        elif request.Has(vtk.vtkDemandDrivenPipeline.REQUEST_DATA()):
             inp = inInfo[0].GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
             out = outInfo.GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
-
             out.ShallowCopy(inp)
 
-
+        #elif request.Has(vtk.vtkDemandDrivenPipeline.REQUEST_UPDATE_TIME()):
+         #   print request
+        #else:
+        #    print '\n'.join(dir(request))
+            #info = outInfo.GetInformationObject(0)
+            #t = info.Get(vtk.vtkStreamingDemandDrivenPipeline.UPDATE_TIME_STEP())
+            #info.Set(vtk.vtkStreamingDemandDrivenPipeline.UPDATE_TIME_STEP(), 0.55)
 
         return 1
 
@@ -28,6 +37,25 @@ class TimeSeries(object):
 
     def FillOutputPortInformation(self, vtkself, port, info):
         info.Set(vtk.vtkDataObject.DATA_TYPE_NAME(), "vtkMultiBlockDataSet")
+        return 1
+"""
+
+class TimeSeries(VTKPythonAlgorithmBase):
+    def __init__(self):
+        VTKPythonAlgorithmBase.__init__(self,
+                                        nInputPorts=1, inputType='vtkMultiBlockDataSet',
+                                        nOutputPorts=1, outputType='vtkMultiBlockDataSet')
+
+
+    def RequestData(self, request, inInfo, outInfo):
+        data_in = inInfo[0].GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
+
+        data_out = outInfo.GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
+        #inp = inInfo.GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
+        #out = outInfo[0].GetInformationObject(0).Get(vtk.vtkDataObject.DATA_OBJECT())
+        #out.ShallowCopy(inp)
+
+        data_out.ShallowCopy(data_in)
         return 1
 
 
@@ -43,19 +71,17 @@ reader.SetTimeStep(10)
 reader.SetAllArrayStatus(vtk.vtkExodusIIReader.NODAL, 1)
 reader.Update();
 
-series = vtk.vtkPythonAlgorithm()
-series.SetPythonObject(TimeSeries())
+#series = vtk.vtkPythonAlgorithm()
+#series.SetPythonObject(TimeSeries())
+series = TimeSeries()
 series.SetInputConnection(reader.GetOutputPort())
-
-#series = vtk.vtkMultiBlockFromTimeSeriesFilter()
-#series.SetInputConnection(reader.GetOutputPort())
-#series.Update(); print series
 
 # Time interpolation (How do I set this up?)
 #time = vtk.vtkTemporalInterpolator()
 #time.SetInputConnection(series.GetOutputPort())
 #time.SetResampleFactor(2)
 #time.Update()
+
 
 
 # Create Geometry
