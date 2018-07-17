99d30a12033c4e44d6a3876c547ec613f225a7a7
Andrew E Slaughter <andrew.slaughter@inl.gov>
2018-07-17 07:37:19 -0600

    foo

--- python/chigger/vtk_scripts/plane_2d.py
+++ python/chigger/vtk_scripts/plane_2d.py
@@ -2,32 +2,7 @@
 import math
 import vtk
 
-
-
 source = vtk.vtkPlaneSource()
-
-#p = [0, 0]
-source.SetOrigin(0.25, 0.5, 0.)
-source.SetPoint1(0.5, 0.25, 0.)
-source.SetPoint2(0.5, 0.75, 0.)
-
-#source.SetOrigin(0., 0.5, 0.)
-#source.SetPoint1(0.5, 0.0, 0.)
-#source.SetPoint2(0.5, 1.0, 0.)
-
-p = source.GetCenter()
-
-
-# Fills screen
-#p = [1, 1]
-#source.SetOrigin(-0.5, 0, 0)
-#source.SetPoint1(0, -0.5, 0)
-#source.SetPoint2(0, 0.5, 0)
-
-#source.SetOrigin(10,10,0)
-#source.SetPoint1(490, 10, 0)
-#source.SetPoint2(10, 490, 0)
-
 #mapper = vtk.vtkPolyDataMapper2D()
 mapper = vtk.vtkPolyDataMapper()
 mapper.SetInputConnection(source.GetOutputPort())
@@ -41,84 +16,91 @@ renderer = vtk.vtkRenderer()
 renderer.AddViewProp(actor)
 
 # Window and Interactor
-window = vtk.vtkRenderWindow()
-window.AddRenderer(renderer)
-window.SetSize(500, 500)
+vtk_window = vtk.vtkRenderWindow()
+vtk_window.AddRenderer(renderer)
+vtk_window.SetSize(400, 400)
 
-camera = renderer.MakeCamera()
-renderer.SetActiveCamera(camera)
+vtkcamera = renderer.MakeCamera()
+renderer.SetActiveCamera(vtkcamera)
+
+origin = [0.5, 0.75, 0]
+p1 = [0.75, 0.5, 0]
+p2 = [0.75, 1, 0]
 
 # Coordinate transformation object
 tr = vtk.vtkCoordinate()
 tr.SetCoordinateSystemToNormalizedViewport()
 
+window = renderer.GetRenderWindow().GetSize()
+
+#source.SetOrigin(origin[0]*window[0], origin[1]*window[1], 0)
+#source.SetPoint1(p1[0]*window[0], p1[1]*window[1], 0)
+#source.SetPoint2(p2[0]*window[0], p2[1]*window[1], 0)
+source.SetOrigin(origin[0], origin[1], 0)
+source.SetPoint1(p1[0], p1[1], 0)
+source.SetPoint2(p2[0], p2[1], 0)
+
+
+p = source.GetOrigin()#[0.5, 0.75]
+w = 0.5
+
+
 # Size of window
-window_size = renderer.GetRenderWindow().GetSize()
-print window_size
 
 # Size of image
 bnds = mapper.GetBounds()
 size = [bnds[1] - bnds[0], bnds[3] - bnds[2]]
-print 'size =', size
-###size = mapper.GetBounds()#[1, 1] #source.GetOutput().GetDimensions()
-
+#w = float(size[0])/float(window[0])
+#size = self._sources[-1].getVTKSource().GetOutput().GetDimensions()
 
 # Normalized image width
-#scale = 1#float(window[0])/float(size[0]) * self.getOption('width')
-scale = float(window_size[0])/float(size[0])# * size[0]#self.getOption('width')
-print 'scale = ', scale
+scale = float(window[0])/float(size[0]) * w
 
 # Compute the camera distance
-angle = camera.GetViewAngle()
-print 'angle =', angle
-
-#d = window_size[1]*0.5 / math.tan(math.radians(angle*0.5))
-d = window_size[1]*0.5 / math.tan(math.radians(angle*0.5))
-print 'distance =', d
+angle = vtkcamera.GetViewAngle()
+d = window[1]*0.5 / math.tan(math.radians(angle*0.5))
+print size, d, scale
 
 # Determine the image position
 tr.SetValue(p[0], p[1], 0)
 position = list(tr.GetComputedDisplayValue(renderer))
-print 'position =', position
 
+# Adjust for off-center alignments
+#if self.getOption('horizontal_alignment') == 'left':
+position[0] = position[0] + (size[0]*0.5*scale)
+#elif self.getOption('horizontal_alignment') == 'right':
+#    position[0] = position[0] - (size[0]*0.5*scale)
+#
+#if self.getOption('vertical_alignment') == 'top':
+#position[1] = position[1] - (size[1]*0.5*scale)
+#elif self.getOption('vertical_alignment') == 'bottom':
+position[1] = position[1] + (size[1]*0.5*scale)
 
 # Reference position (middle of window)
 tr.SetValue(0.5, 0.5, 0)
 ref = tr.GetComputedDisplayValue(renderer)
 
-#print ref
-
-
 # Camera offsets
 x = (ref[0] - position[0]) * 1/scale
 y = (ref[1] - position[1]) * 1/scale
 
-print 'x = {}; y = {}'.format(x, y)
-
 # Set the camera
-
-camera.SetViewUp(0, 1, 0)
-camera.SetPosition(size[0] + x, size[1] + y, d * 1/scale)
-#camera.SetFocalPoint(size[0]/2. + x, size[1]/2. + y, 0)
-camera.SetFocalPoint(source.GetCenter())
-#camera.SetFocalPoint(0,0,0)
-
-print 'pos = ', camera.GetPosition()
-print camera, source
+vtkcamera.SetViewUp(0, 1, 0)
+vtkcamera.SetPosition(size[0]/2. + x, size[1]/2. + y, d * 1/scale)
+vtkcamera.SetFocalPoint(size[0]/2. + x, size[1]/2. + y, 0)
 
 # Update the renderer
-#renderer.ResetCameraClippingRange()
-
+renderer.ResetCameraClippingRange()
 
 
 
 
 
 interactor = vtk.vtkRenderWindowInteractor()
-interactor.SetRenderWindow(window)
+interactor.SetRenderWindow(vtk_window)
 interactor.Initialize()
 
 # Show the result
-window.Render()
+vtk_window.Render()
 interactor.Start()
-print camera
+#print camera
