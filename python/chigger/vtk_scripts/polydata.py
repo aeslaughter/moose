e6d1a3c68668b516a40f60a33c3f5057c8fc99ea
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-05-17 05:11:29 -0600

    2D rectangle script now has color and arbitrary layers

--- python/chigger/vtk_scripts/polydata.py
+++ python/chigger/vtk_scripts/polydata.py
@@ -1,53 +1,56 @@
 #!/usr/bin/env python
 import vtk
+import numpy as np
 
 name = 'data'
+n = 256
+point1 = [0.25, 0.25]
+point2 = [0.5, 0.75]
+
+ydata = np.linspace(point1[1], point2[1], n+1)
 
 # Define points
 points = vtk.vtkPoints()
-points.InsertNextPoint(.25, .25, 0)
-points.InsertNextPoint(.5, .25, 0)
-points.InsertNextPoint(.5, .5, 0)
-points.InsertNextPoint(.25, .5, 0)
-points.InsertNextPoint(.5, .75, 0)
-points.InsertNextPoint(.25, .75, 0)
+points.SetNumberOfPoints(len(ydata)*2)
 
-# Define two quads, one above the other
-quad = vtk.vtkQuad()
-quad.GetPointIds().SetId(0, 0)
-quad.GetPointIds().SetId(1, 1)
-quad.GetPointIds().SetId(2, 2)
-quad.GetPointIds().SetId(3, 3)
-
-quad2 = vtk.vtkQuad()
-quad2.GetPointIds().SetId(0, 3)
-quad2.GetPointIds().SetId(1, 2)
-quad2.GetPointIds().SetId(2, 4)
-quad2.GetPointIds().SetId(3, 5)
+data = vtk.vtkFloatArray()
+data.SetName(name)
+data.SetNumberOfTuples(len(ydata)*2)
 
 cells = vtk.vtkCellArray()
-cells.InsertNextCell(quad)
-cells.InsertNextCell(quad2)
+#cells.SetNumberOfCells(n)
+
+idx = 0
+for i, y in enumerate(ydata):
+    points.SetPoint(idx, point1[0], y, 0)
+    points.SetPoint(idx+1, point2[0], y, 0)
+
+    value = float(i)/(n)
+    data.SetValue(idx, value)
+    data.SetValue(idx+1, value)
+
+    if idx > 0:
+        quad = vtk.vtkQuad()
+        quad.GetPointIds().SetId(0, idx-2)
+        quad.GetPointIds().SetId(1, idx-1)
+        quad.GetPointIds().SetId(2, idx+1)
+        quad.GetPointIds().SetId(3, idx)
+        cells.InsertNextCell(quad)
+        #cells.InsertNextCell(vtk.VTK_QUAD, [idx-2, idx-1, idx, idx+1])
+    idx += 2
 
 poly = vtk.vtkPolyData()
 poly.SetPoints(points)
 poly.SetPolys(cells)
-
-# Populate the cell data
-data = vtk.vtkFloatArray()
-data.SetName(name)
-data.SetNumberOfTuples(2)
-for i in xrange(2):
-    data.SetValue(i, i)
-poly.GetCellData().AddArray(data)
+poly.GetPointData().AddArray(data)
 
 coordinate = vtk.vtkCoordinate()
 coordinate.SetCoordinateSystemToNormalizedDisplay()
 
 mapper = vtk.vtkPolyDataMapper2D()
 mapper.SetInputData(poly)
-mapper.SetScalarModeToUseCellFieldData()
-mapper.SetScalarRange(data.GetRange(0))
+mapper.SetScalarModeToUsePointFieldData()
+#mapper.SetScalarRange(0, 1)
 mapper.SetTransformCoordinate(coordinate)
 mapper.ColorByArrayComponent(name, 0)
 
