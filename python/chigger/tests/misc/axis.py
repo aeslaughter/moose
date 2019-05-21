6a4a7a1baf3e41aef6dde701d82c0b2668e0ed50
Andrew E Slaughter <andrew.slaughter@inl.gov>
2019-05-20 21:40:29 -0600

    wip

--- python/chigger/tests/misc/axis.py
+++ python/chigger/tests/misc/axis.py
@@ -23,7 +23,8 @@ ax = misc.Axis2D(title='Axis Title',
                  #major_nticks=7,
                  major_offset=18,
                  minor_nticks=3,
-                 color=(0,0,1))
+                 color=(0,0,1),
+                 title_orientation=45)
 
 view = chigger.Viewport(ax)
 window = chigger.Window(view, size=(1200,600), background=(1,1,1))
