5e3f61d64aa9d0237dd41de94d91b4816cb2a519
Andrew E Slaughter <slaughter98@gmail.com>
2021-09-01 18:30:56 -0600

    WIP: edge conform

--- test/tests/meshgenerators/elevation/mesh_elevations.py
+++ test/tests/meshgenerators/elevation/mesh_elevations.py
@@ -11,7 +11,7 @@ import itertools
 import dataclasses
 
 class ElementType(enum.IntEnum):
-    POINT = 0
+    POINT = 15
     LINE2 = 1
     TRI3 = 2
     QUAD4 = 3
@@ -50,7 +50,6 @@ def build_surface(file_name, elem_size, elem_type=ElementType.TRI3):
     grid_x, grid_y = numpy.meshgrid(xi, yi)
     grid_z = scipy.interpolate.griddata((points['x'], points['y']), points['z'], (grid_x, grid_y), method='nearest')
 
-
     nid = get_node_tag()
     nodes = numpy.ndarray((nx, ny), dtype=Node)
     for i in range(nx):
@@ -58,19 +57,61 @@ def build_surface(file_name, elem_size, elem_type=ElementType.TRI3):
             nodes[i,j] = Node(tag=int(nid), coords=[grid_x[i, j], grid_y[i, j], grid_z[i, j]])
             nid += 1
 
+    # Create 4 discrete points for the 4 corners of the terrain surface
+    pt0 = gmsh.model.addDiscreteEntity(0)
+    gmsh.model.setCoordinates(pt0, *nodes[0,0].coords)
+
+    pt1 = gmsh.model.addDiscreteEntity(0)
+    gmsh.model.setCoordinates(pt1, *nodes[0,-1].coords)
+
+    pt2 = gmsh.model.addDiscreteEntity(0)
+    gmsh.model.setCoordinates(pt2, *nodes[-1,-1].coords)
+
+    pt3 = gmsh.model.addDiscreteEntity(0)
+    gmsh.model.setCoordinates(pt3, *nodes[-1,0].coords)
+
+    # Create 4 discrete bounding curves, with their boundary points:
+    bnd0 = gmsh.model.addDiscreteEntity(1, boundary=[pt0, pt1])
+    bnd1 = gmsh.model.addDiscreteEntity(1, boundary=[pt1, pt2])
+    bnd2 = gmsh.model.addDiscreteEntity(1, boundary=[pt2, pt3])
+    bnd3 = gmsh.model.addDiscreteEntity(1, boundary=[pt3, pt0])
+
+    # Create one discrete surface, with its bounding curves:
+    surface = gmsh.model.addDiscreteEntity(2, boundary=[bnd0, bnd1, bnd2, bnd3])
+
+    gmsh.model.mesh.addNodes(2, surface, [n.tag for n in nodes.flat],
+                             list(itertools.chain.from_iterable(n.coords for n in nodes.flat)))
+
+
+    # Mesh corners
+    gmsh.model.mesh.addElementsByType(pt0, ElementType.POINT, [], [nodes[0,0].tag])
+    gmsh.model.mesh.addElementsByType(pt1, ElementType.POINT, [], [nodes[0,-1].tag])
+    gmsh.model.mesh.addElementsByType(pt2, ElementType.POINT, [], [nodes[-1,-1].tag])
+    gmsh.model.mesh.addElementsByType(pt3, ElementType.POINT, [], [nodes[-1,0].tag])
+
+    gmsh.model.mesh.addElementsByType(bnd0, ElementType.LINE2, [], [nodes[0,0].tag, nodes[0,-1].tag])
+    gmsh.model.mesh.addElementsByType(bnd1, ElementType.LINE2, [], [nodes[0,-1].tag, nodes[-1,-1].tag])
+    gmsh.model.mesh.addElementsByType(bnd2, ElementType.LINE2, [], [nodes[-1,-1].tag, nodes[-1,0].tag])
+    gmsh.model.mesh.addElementsByType(bnd3, ElementType.LINE2, [], [nodes[-1,0].tag, nodes[0,0].tag])
+
+
+
     elements = []
     for i in range(nx-1):
         for j in range(ny-1):
             elements += [nodes[i,j].tag, nodes[i+1, j].tag, nodes[i+1,j+1].tag]
             elements += [nodes[i,j].tag, nodes[i+1, j+1].tag, nodes[i,j+1].tag]
 
-    surface = gmsh.model.addDiscreteEntity(2)
-
-    gmsh.model.mesh.addNodes(2, surface, [],
-                             list(itertools.chain.from_iterable(n.coords for n in nodes.flat)))
-
-    # 2 -> Triangle
     gmsh.model.mesh.addElementsByType(surface, ElementType.TRI3, [], elements)
+    #print(gmsh.model.getEntities(2))
+    #print(gmsh.model.getEntities(1))
+    #gmsh.model.mesh.reclassifyNodes()
+    gmsh.model.mesh.createGeometry()
+    #print(gmsh.model.getEntities(2))
+    #print(gmsh.model.getEntities(1))
+
+
+
     return nodes, surface
 
 
@@ -80,28 +121,9 @@ def build_sides(bot_nodes, top_nodes, bot_elem_size=0, top_elem_size=0):
     surfaces = list()
 
 
+    # X=0 face: i=0, j
     top_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=top_elem_size) for n in top_nodes[0,:]]
     bot_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=bot_elem_size) for n in bot_nodes[0,:]]
-
-    # X=0 face: i=0, j
-    #surface = gmsh.model.addDiscreteEntity(2)
-    #gmsh.model.mesh.addNodes(2, surface, [],
-    #                         list(itertools.chain.from_iterable(n.coords for n in top_nodes[0,:].flat)))
-    #gmsh.model.mesh.addNodes(2, surface, [],
-    #                         list(itertools.chain.from_iterable(n.coords for n in bot_nodes[0,:].flat)))
-
-
-
-    #elements = list()
-    #for j in range(top_nodes.shape[1] - 1):
-    #    elements += [top_nodes[0,j].tag, top_nodes[0,j+1].tag]
-
-
-
-    #gmsh.model.mesh.addElementsByType(surface, ElementType.LINE2, [], elements)
-    #surfaces.append(surface)
-
-
     line_top = gmsh.model.geo.addPolyline(top_points)
     line_bot = gmsh.model.geo.addPolyline(bot_points)
     line_right = gmsh.model.geo.addLine(bot_points[0], top_points[0])
@@ -109,44 +131,40 @@ def build_sides(bot_nodes, top_nodes, bot_elem_size=0, top_elem_size=0):
     curve = gmsh.model.geo.addCurveLoop([line_top, -line_left, -line_bot, line_right])
     surfaces.append(gmsh.model.geo.addPlaneSurface([curve]))
 
-    gmsh.model.geo.synchronize()
-    gmsh.model.mesh.embed(0, top_points, 2, surfaces[-1])
-
-    #gmsh.model.geo.synchronize()
-    #gmsh.model.mesh.embed(0, top_points, 2, surfaces[-1])
-
-    #gmsh.model.geo.synchronize()
-    #gmsh.model.mesh.embed(0, bot_points, 2, surfaces[-1])
-
-
-    #gmsh.model.geo.synchronize()
-    #gmsh.model.mesh.embed(1, [curve], 2, surfaces[-1])
-
-    """
     # X=-1 face: i=-1, j
-    line_top = gmsh.model.geo.addPolyline(top_nodes[-1,:].tag)
-    line_bot = gmsh.model.geo.addPolyline(bot_nodes[-1,:].tag)
-    line_right = gmsh.model.geo.addLine(bot_nodes[-1,-1].tag, top_nodes[-1,-1].tag)
-    line_left = gmsh.model.geo.addLine(bot_nodes[-1,0].tag, top_nodes[-1,0].tag)
-    curve = gmsh.model.geo.addCurveLoop([line_top, -line_right, -line_bot, line_left])
+    top_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=top_elem_size) for n in top_nodes[-1,:]]
+    bot_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=bot_elem_size) for n in bot_nodes[-1,:]]
+    line_top = gmsh.model.geo.addPolyline(top_points)
+    line_bot = gmsh.model.geo.addPolyline(bot_points)
+    line_right = gmsh.model.geo.addLine(bot_points[0], top_points[0])
+    line_left = gmsh.model.geo.addLine(bot_points[-1], top_points[-1])
+    curve = gmsh.model.geo.addCurveLoop([line_top, -line_left, -line_bot, line_right])
+    #curve = gmsh.model.geo.addCurveLoop([line_top, -line_right, -line_bot, line_left])
     surfaces.append(gmsh.model.geo.addPlaneSurface([curve]))
 
     # Y=0 face: i, j=0
-    line_top = gmsh.model.geo.addPolyline(top_nodes[:,0].tag)
-    line_bot = gmsh.model.geo.addPolyline(bot_nodes[:,0].tag)
-    line_right = gmsh.model.geo.addLine(bot_nodes[-1,0].tag, top_nodes[-1,0].tag)
-    line_left = gmsh.model.geo.addLine(bot_nodes[0,0].tag, top_nodes[0,0].tag)
-    curve = gmsh.model.geo.addCurveLoop([line_top, -line_right, -line_bot, line_left])
+    top_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=top_elem_size) for n in top_nodes[:,0]]
+    bot_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=bot_elem_size) for n in bot_nodes[:,0]]
+    line_top = gmsh.model.geo.addPolyline(top_points)
+    line_bot = gmsh.model.geo.addPolyline(bot_points)
+    line_right = gmsh.model.geo.addLine(bot_points[0], top_points[0])
+    line_left = gmsh.model.geo.addLine(bot_points[-1], top_points[-1])
+    curve = gmsh.model.geo.addCurveLoop([line_top, -line_left, -line_bot, line_right])
+
+#    curve = gmsh.model.geo.addCurveLoop([line_top, -line_right, -line_bot, line_left])
     surfaces.append(gmsh.model.geo.addPlaneSurface([curve]))
 
     # Y=-1 face: i, j=01
-    line_top = gmsh.model.geo.addPolyline(top_nodes[:,-1].tag)
-    line_bot = gmsh.model.geo.addPolyline(bot_nodes[:,-1].tag)
-    line_right = gmsh.model.geo.addLine(bot_nodes[0,-1].tag, top_nodes[0,-1].tag)
-    line_left = gmsh.model.geo.addLine(bot_nodes[-1,-1].tag, top_nodes[-1,-1].tag)
+    top_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=top_elem_size) for n in top_nodes[:,-1]]
+    bot_points = [gmsh.model.geo.addPoint(*n.coords, meshSize=bot_elem_size) for n in bot_nodes[:,-1]]
+    line_top = gmsh.model.geo.addPolyline(top_points)
+    line_bot = gmsh.model.geo.addPolyline(bot_points)
+    line_right = gmsh.model.geo.addLine(bot_points[0], top_points[0])
+    line_left = gmsh.model.geo.addLine(bot_points[-1], top_points[-1])
     curve = gmsh.model.geo.addCurveLoop([line_top, -line_left, -line_bot, line_right])
+
+    #curve = gmsh.model.geo.addCurveLoop([line_top, -line_left, -line_bot, line_right])
     surfaces.append(gmsh.model.geo.addPlaneSurface([curve]))
-    """
 
 
     return surfaces
@@ -162,7 +180,7 @@ def main():
 
     surfaces = [bot_surface, mid_surface, top_surface]
 
-    surfaces += build_sides(bot_nodes, mid_nodes, 500, 100)
+    #surfaces += build_sides(bot_nodes, mid_nodes, 500, 100)
     #surfaces += build_sides(mid_nodes, top_nodes)
 
     #faces = gmsh.model.geo.addSurfaceLoop(surfaces)
--- test/tests/meshgenerators/elevation/rectangle.py
+++ test/tests/meshgenerators/elevation/rectangle.py
@@ -5,30 +5,39 @@ import math
 
 def surface():
     """This works like I would expect with element vertex at points"""
-    n_top = 20
+    n_top = 10
+    n_mid = 20
     n_bot = 10
 
     gmsh.initialize()
     gmsh.model.add('surface')
 
-    top_y_func = lambda x: 1 + 0.1*math.sin(math.pi*x)
-    top_points = [gmsh.model.geo.addPoint(i/n_top, top_y_func(i/n_top), 0, 1/(n_top-1)) for i in range(n_top+1)]
-    #top_points = [gmsh.model.geo.addPoint(i/n_top, top_y_func(i/n_top), 0, 1/(n_top+1)) for i in range(n_top+1)]
-    #top_points = [gmsh.model.geo.addPoint(i/n_top, top_y_func(i/n_top), 0, 1/n_top) for i in range(n_top+1)]
+    mid_y_func = lambda x: 1 + 0.1*math.sin(math.pi*x)
+
+    top_points = [gmsh.model.geo.addPoint(i/n_top, 2, 0, 1/n_top) for i in range(n_top+1)]
+    mid_points = [gmsh.model.geo.addPoint(i/n_top, mid_y_func(i/n_top), 0, 1/n_top) for i in range(n_mid+1)]
     bot_points = [gmsh.model.geo.addPoint(i/n_bot, 0, 0, 1/n_bot) for i in range(n_bot+1)]
 
-    line_top = gmsh.model.geo.addPolyline(top_points)
-    line_bot = gmsh.model.geo.addPolyline(bot_points)
-    line_right = gmsh.model.geo.addLine(bot_points[-1], top_points[-1])
-    line_left = gmsh.model.geo.addLine(bot_points[0], top_points[0])
 
-    curve = gmsh.model.geo.addCurveLoop([line_top, -line_right, -line_bot, line_left])
+
+
+    """
+    lines = []
+    for i in range(n_top):
+        lines.append(gmsh.model.geo.addLine(top_points[i], top_points[i+1]))
+    lines.append(gmsh.model.geo.addLine(top_points[-1], bot_points[-1]))
+
+    for i in range(n_bot, 0, -1):
+        lines.append(gmsh.model.geo.addLine(bot_points[i], bot_points[i-1]))
+
+    lines.append(gmsh.model.geo.addLine(bot_points[0], top_points[0]))
+    """
+
+    curve = gmsh.model.geo.addCurveLoop(lines)
     surface = gmsh.model.geo.addPlaneSurface([curve])
 
     gmsh.model.geo.synchronize()
 
-    #https://gitlab.onelab.info/gmsh/gmsh/blob/gmsh_4_8_4/tutorial/python/t15.py#L47
-    gmsh.model.mesh.embed(0, top_points, 2, surface) # this does nothing
     gmsh.model.mesh.generate()
 
     gmsh.fltk.run()
